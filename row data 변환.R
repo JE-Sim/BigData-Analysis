a3a3091c9e04b3dc72f15df02639961e4068dcd9
dlgpwls26 <33315680+dlgpwls26@users.noreply.github.com>
2017-11-16 15:26:23 +0900

    smoking
    smoking country code 넣은게 final

--- row data 변환.R
+++ row data 변환.R
@@ -103,3 +103,22 @@ obesity.recent <- recentYear(Obesity1)
 write.csv(obesity.2016, "Obesity.2016.csv")
 
 #changed
+
+# smoking
+setwd("C:/Users/dlgpw/Desktop")
+Smoking <- read.csv("Smoking.revised.csv", header = T)
+country.code <- read.csv("국가코드 완성.csv", header = T)
+
+Smoking <- Smoking[order(Smoking$Country.Name),]
+
+c.name <- as.character(country.code$Country.Name)
+o.name <- as.character(Smoking$Country.Name)
+common <- intersect(c.name, o.name)
+country.code <- country.code[order(country.code$Country.Name),]
+
+o.name1 <- country.code[country.code$Country.Name %in% common,]
+Smoking0 <- Smoking[Smoking $ Country.Name %in% common,]
+Smoking1 <- cbind(o.name1, Smoking0[,-1])
+
+
+write.csv(Smoking1, "Smoking.final.CSV", row.names = F)
